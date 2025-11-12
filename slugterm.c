714bca28263eb4ce846ebfe24412f2a7bd233936
SkenorDev <nathanskinner@comcast.net>
2025-11-11 16:36:34 -0800

    Add files via upload

--- slugterm.c
+++ slugterm.c
@@ -3,58 +3,84 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-//https://stackoverflow.com/questions/4235519/counting-number-of-occurrences-of-a-char-in-a-string-in-c
+#define BUFFER_SIZE 4096
+#define MAX_ARGS    100
+
+void parse_simple_cmd(char *cmd, char **argv) {
+    int i = 0;
+    char *token = strtok(cmd, " ");
+    while (token != NULL && i < MAX_ARGS - 1) {
+        argv[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[i] = NULL; // argv must be NULL-terminated
+}
+
 int countChars(char *s, char c) {
     return *s == '\0' ? 0 : countChars(s + 1, c) + (*s == c);
 }
 
 int main(void) {
+
     while (1) {
         printf("slugterm>");
+        fflush(stdout); // Make sure prompt is printed immediately
 
-        const size_t buffer_size = 4096;
-        char buffer[4096];
-        char *input = fgets(buffer, buffer_size, stdin);
+        char buffer[BUFFER_SIZE];
+        char *input = fgets(buffer, BUFFER_SIZE, stdin);
+        // leave if nothing
         if (input == NULL) {
             printf("\n");
             break;
         }
-        input[strcspn(input, "\n")] = '\0';
 
-        // Get number of commands
-        const int commandCount = countChars(input, '|') + 1;
+        buffer[strcspn(buffer, "\n")] = '\0'; // Remove trailing newline
 
+        int commandCount = countChars(buffer, '|') + 1;
         char **commands = malloc(commandCount * sizeof(char *));
-        const char *delim = "|";
-        char *command = strtok(input, delim);
 
         int i = 0;
-        while (command != NULL && i < commandCount) {
-            commands[i] = command;
-            i++;
-            command = strtok(NULL, delim);
+
+        // Create pipes
+        int pipes[commandCount - 1][2];
+        for (i = 0; i < commandCount - 1; i++) {
+            pipe(pipes[i]);
+        }
+        pid_t pids[commandCount];
+        for (i = 0; i < commandCount; i++) {
+            pids[i] = fork();
+            if (pids[i] == 0) {
+                if (i >= commandCount - 1) {
+                    dup2(pipes[i][1], STDOUT_FILENO);
+                } else {
+                    dup2(pipes[i - 1][0], STDOUT_FILENO);
+                }
+
+                // close non process pipes
+                for (int j = 0; j < commandCount - 1; j++) {
+                    close(pipes[j][0]);
+                    close(pipes[j][1]);
+                }
+                // parse and execute
+                char *argv[MAX_ARGS];
+                parse_simple_cmd(commands[i], argv);
+                execv(argv[0], argv);
+                perror("execv");
+                _exit(1);
+            }
+        }
+        //PARENT STARTS
+
+        for (i = 0; i < commandCount - 1; i++) {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
         }
 
-        // testing the parsing
-        // for (int j = 0; j < commandCount; j++) {
-        //     printf("Command %d: '%s'\n", j + 1, commands[j]);
-        // }
-
-        int fd[2];
-        pipe(fd);
-        pid_t pid = fork();
-        //child
-        if (pid == 0) {
-            if (commandCount == 1) {
-                execv(commands[0], NULL);
-            }
-
-            exit(1);
-        } else //parent
-        {
+        for (i = 0; i < commandCount; i++) {
             int status;
-            waitpid(pid, &status, 0);
+            waitpid(pids[i], &status, 0);
         }
         free(commands);
     }
