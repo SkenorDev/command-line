5086996a57fa3e84e0718d1354bac5920b89abf3
SkenorDev <nathanskinner@comcast.net>
2025-11-12 16:30:23 -0800

    Add files via upload

--- slugterm.c
+++ slugterm.c
@@ -7,7 +7,14 @@
 
 #define BUFFER_SIZE 4096
 #define MAX_ARGS    100
-
+// python3 -m autograder.run.submit slugterm.c Makefile
+void print_commands(char **commands, int commandCount) {
+    printf("Commands (%d):\n", commandCount);
+    for (int i = 0; i < commandCount; i++) {
+        printf("  [%d] %s\n", i, commands[i]);
+    }
+}
+//CHATGPT
 void parse_simple_cmd(char *cmd, char **argv) {
     int i = 0;
     char *token = strtok(cmd, " ");
@@ -23,10 +30,10 @@ int countChars(char *s, char c) {
 }
 
 int main(void) {
-
     while (1) {
+        fflush(stdout);
         printf("slugterm>");
-        fflush(stdout); // Make sure prompt is printed immediately
+        fflush(stdout);
 
         char buffer[BUFFER_SIZE];
         char *input = fgets(buffer, BUFFER_SIZE, stdin);
@@ -40,9 +47,16 @@ int main(void) {
 
         int commandCount = countChars(buffer, '|') + 1;
         char **commands = malloc(commandCount * sizeof(char *));
-
+        fprintf(stderr, "%d\n", commandCount);
         int i = 0;
+        char *token = strtok(buffer, "|");
+        while (token != NULL && i < commandCount) {
+            commands[i++] = token;
+            token = strtok(NULL, "|");
+        }
 
+        // print_commands(commands, commandCount);
+        // fflush(stdout);
         // Create pipes
         int pipes[commandCount - 1][2];
         for (i = 0; i < commandCount - 1; i++) {
@@ -52,10 +66,13 @@ int main(void) {
         for (i = 0; i < commandCount; i++) {
             pids[i] = fork();
             if (pids[i] == 0) {
-                if (i >= commandCount - 1) {
-                    dup2(pipes[i][1], STDOUT_FILENO);
-                } else {
+                //If not first pipe write behind
+                if (i > 0) {
                     dup2(pipes[i - 1][0], STDOUT_FILENO);
+                    //If not final pipe write ahead
+                }
+                if (i < commandCount - 1) {
+                    dup2(pipes[i][1], STDOUT_FILENO);
                 }
 
                 // close non process pipes
@@ -66,11 +83,14 @@ int main(void) {
                 // parse and execute
                 char *argv[MAX_ARGS];
                 parse_simple_cmd(commands[i], argv);
-                execv(argv[0], argv);
+                fprintf(stderr, "input:%s\n", commands[i]);
+                fflush(stdout);
+                execvp(argv[0], argv);
                 perror("execv");
                 _exit(1);
             }
         }
+
         //PARENT STARTS
 
         for (i = 0; i < commandCount - 1; i++) {
@@ -78,7 +98,7 @@ int main(void) {
             close(pipes[i][1]);
         }
 
-        for (i = 0; i < commandCount; i++) {
+        for (i = 0; i < commandCount - 1; i++) {
             int status;
             waitpid(pids[i], &status, 0);
         }
